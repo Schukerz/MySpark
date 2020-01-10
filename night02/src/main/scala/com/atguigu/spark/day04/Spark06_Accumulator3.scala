9bf0af9b889ebeda34642559f9c7b027c13b737d
flash <flash@flash1.com>
2020-01-10 11:40:08 +0800

    day04

---
+++ night02/src/main/scala/com/atguigu/spark/day04/Spark06_Accumulator3.scala
@@ -0,0 +1,34 @@
+package com.atguigu.spark.day04
+
+import org.apache.spark.util.AccumulatorV2
+
+class Spark06_Accumulator3 extends AccumulatorV2[Long,Map[String,Double]] {
+  private var map = Map[String,Double]()
+  override def isZero: Boolean = map.isEmpty
+
+  override def copy(): AccumulatorV2[Long, Map[String, Double]] = {
+    val acc = new Spark06_Accumulator3()
+    acc.map = this.map
+    acc
+  }
+
+  override def reset(): Unit = {map = Map[String,Double]()}
+
+  override def add(v: Long): Unit = {
+    map += "sum" -> (map.getOrElse("sum",0D)+v)
+    map += "count" -> (map.getOrElse("count",0D) +1)
+  }
+
+  override def merge(other: AccumulatorV2[Long, Map[String, Double]]): Unit = other match {
+    case s : Spark06_Accumulator3 => {
+      this.map += "sum" ->(this.map.getOrElse("sum",0D) + s.map.getOrElse("sum",0D))
+      this.map += "count" ->(this.map.getOrElse("count",0D) + s.map.getOrElse("count",0D))
+    }
+    case _ => throw new UnsupportedOperationException
+  }
+
+  override def value: Map[String, Double] = {
+    map += "avg" -> this.map.getOrElse("sum",0D)/this.map.getOrElse("count",0D)
+    map
+  }
+}
---
+++ night02/src/main/scala/com/atguigu/spark/day04/Spark07_TextAccumulator3.scala
@@ -0,0 +1,24 @@
+package com.atguigu.spark.day04
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark07_TextAccumulator3 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Spark07_TextAccumulator3").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val acc = new Spark06_Accumulator3
+    sc.register(acc,"MapAcc")
+    val list: List[(String, Int)] = List(("a",1),("b",1),("c",2),("d",2))
+    val rdd: RDD[(String, Int)] = sc.parallelize(list,2)
+    rdd.map(x=>{
+      acc.add(x._2)
+      x
+    }).collect()
+    println(acc)
+    sc.stop()
+
+    
+
+  }
+}
